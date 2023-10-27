b946e1fbaeff2d1b8d6afc970bda0165dcb521c6
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-10-27 15:15:56 -0400

    Initial commit for DOM practice

---
+++ dompractice.js
@@ -0,0 +1,85 @@
+// ===== JavaScript =====
+// Change the title of your webpage to 'DOM Practice' using textContent and without assigning an id or a class name
+const title = document.querySelector("title")
+title.textContent = "DOM Practice"
+console.log(title)
+
+
+// Access the parent and apply the following properties to the parent.
+// width 500px
+// height of 450 px
+// background color of red
+const parent = document.querySelector("#container")
+parent.style.backgroundColor ="red"
+parent.style.width = "500px"
+parent.style.height = "450px"
+console.log(parent)
+
+
+// Access the children of parent and apply the following properties to them
+// Width 100%
+// Height 50px
+// Background color of blue
+// Margins 30 px at top and bottom and 0 on left and right
+// Text align of center
+// Font color of #E0D635
+const children = document.querySelectorAll(".child")
+console.log(children)
+for(let child of children){
+    child.style.backgroundColor = "blue"
+    child.style.width = "100%"
+    child.style.height = "50px"
+    child.style.margin = "30px 0px"
+    child.style.textAlign = "center"
+    child.style.color = "#0D635"
+}
+
+// Access the grandchildren give them the following properties
+// width 100%
+// height of 7px
+// text that indicates index starting at 1
+// margin top of 10px
+
+const grandchildren = document.querySelectorAll(".grandchild")
+for(let i=0; i<grandchildren.length; i++){
+    let grandchild = grandchildren[i]
+    grandchild.style.width = "100%"
+    grandchild.style.height = "7px"
+    grandchild.style.textContent= i+1
+    grandchild.style.margin = "10px 0px 0px 0px"
+    grandchild.textContent = i+1
+}
+
+
+
+
+
+
+
+// Grandma's favorite
+// Access the 14th grandchild and give them a font color of pink and italicize
+// use array index to access the child and use 'children' to access the grandchild.
+// Look at MDN DOM children property
+// Assign a text color purple to the 14th grandchild
+
+
+// The JavaScript of Grandchildren
+// Access the parent with the child named 9 and give the parent a position of relative
+// Access the 9th child and assign it to a variable named blackSheep. Use the same method from above to access the child (children property)
+// Give the following properties to blackSheep
+// text color black
+// font size 9rem
+// text align right
+// transform scale(-1, 1)
+// position absolute
+// top 30px
+// left 500px
+
+
+
+
+
+
+
+
+
